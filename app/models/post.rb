b860c79c2400988ac01a5b8b37d8e05f94b0cc3c
Hyconin <vitaliselyanko@gmail.com>
2023-04-27 00:29:10 +0300

    RoR blog

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates_presence_of :title
+  has_many :comments, dependent: :destroy
+  has_rich_text :content
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
+pin "local-time", to: "https://ga.jspm.io/npm:local_time@2.1.0/app/assets/javascript/local_time.js"
